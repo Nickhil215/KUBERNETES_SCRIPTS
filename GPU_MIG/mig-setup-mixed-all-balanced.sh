0ae47485bcbda662bf12d57f7c30e61a585b0ab5
Nickhil215 <nickhil215@gmail.com>
2025-06-10 12:52:48 +0530

    update

--- GPU_MIG/mig-setup-mixed-all-balanced.sh
+++ GPU_MIG/mig-setup-mixed-all-balanced.sh
@@ -6,9 +6,11 @@
 set -e
 
 NAMESPACE="gpu-operator"
+HELM_RELEASE_NAME="gpu-operator"
 GPU_OPERATOR_VERSION="v25.3.0"
 NODE_NAME="$1"
 MIG_PROFILE="$2"
+GPU_OPERATOR_CHART="nvidia/gpu-operator"
 
 if [ -z "$NODE_NAME" ] || [ -z "$MIG_PROFILE" ]; then
   echo "Usage: $0 <node-name> <mig-profile>"
@@ -16,11 +18,30 @@ if [ -z "$NODE_NAME" ] || [ -z "$MIG_PROFILE" ]; then
   exit 1
 fi
 
-echo "🚀 Installing NVIDIA GPU Operator with MIG Mixed strategy..."
-  helm install --wait --generate-name \
-    -n gpu-operator --create-namespace \
-    nvidia/gpu-operator \
-    --set mig.strategy=mixed
+echo "🚀 Adding NVIDIA container toolkit repo..."
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+apt-get update
+
+echo "🚀 Installing NVIDIA container toolkit..."
+apt-get install -y nvidia-container-toolkit
+
+echo "🚀 Verifying NVIDIA container CLI installation..."
+nvidia-container-cli --version
+
+echo "🚀 Adding NVIDIA Helm repo..."
+helm repo add nvidia https://nvidia.github.io/gpu-operator
+
+echo "🚀 Updating Helm repo..."
+helm repo update
+
+echo "🚀 Enabling the MIG Mode..."
+nvidia-smi -mig 1
+
+echo "🚀 Installing/upgrading NVIDIA GPU Operator with MIG single strategy..."
+helm upgrade --install "$HELM_RELEASE_NAME" "$GPU_OPERATOR_CHART" \
+  -n "$NAMESPACE" --create-namespace \
+  --wait \
+  --set mig.strategy=mixed
 
 echo "⏳ Waiting for pods to initialize..."
 sleep 60  # Adjust as needed for your environment
